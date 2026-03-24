c5025795f971d8f2007e27035e9c65e1c3e6ec58
DevCmb <71194682+29cmb@users.noreply.github.com>
2026-03-24 18:10:48 -0400

    feat: :sparkles: Make the debug topbar display the version and local version (#3)
    * feat: :sparkles: Make the debug topbar display the version and local version
    
    * fix: :bug: remove the version.txt file

--- src/main/kotlin/xyz/devcmb/tumblers/Constants.kt
+++ src/main/kotlin/xyz/devcmb/tumblers/Constants.kt
@@ -2,5 +2,6 @@ package xyz.devcmb.tumblers
 
 data object Constants {
     val IS_DEVELOPMENT = true
-    const val VERSION: String = "1.0.0-00021e"
+    const val VERSION: String = "22e"
+    const val BRANCH: String = "feat/debug_topbar"
 }
