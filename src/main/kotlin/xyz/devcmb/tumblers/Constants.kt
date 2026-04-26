0a81b441005b351cadb80a505e39e7bc5607c417
29cmb <devcmb@outlook.com>
2026-04-25 21:04:51 -0400

    feat: :sparkles: Add all the remaining game dioramas

--- src/main/kotlin/xyz/devcmb/tumblers/Constants.kt
+++ src/main/kotlin/xyz/devcmb/tumblers/Constants.kt
@@ -2,6 +2,6 @@ package xyz.devcmb.tumblers
 
 data object Constants {
     val IS_DEVELOPMENT = true
-    const val VERSION: String = "25a"
+    const val VERSION: String = "268"
     const val BRANCH: String = "master"
 }
