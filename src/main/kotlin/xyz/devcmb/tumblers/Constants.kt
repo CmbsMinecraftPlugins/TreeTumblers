b0c90d7acb2124e5c6dff83c2159a6c32b66eeb6
29cmb <devcmb@outlook.com>
2026-04-29 20:16:12 -0400

    feat: :sparkles: Disable developer mode

--- src/main/kotlin/xyz/devcmb/tumblers/Constants.kt
+++ src/main/kotlin/xyz/devcmb/tumblers/Constants.kt
@@ -1,7 +1,7 @@
 package xyz.devcmb.tumblers
 
 data object Constants {
-    val IS_DEVELOPMENT = true
-    const val VERSION: String = "291"
+    val IS_DEVELOPMENT = false
+    const val VERSION: String = "294"
     const val BRANCH: String = "master"
 }
