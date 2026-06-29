3574058ede74514c0841efa8d7caaafa8c28f70d
29cmb <devcmb@outlook.com>
2026-06-29 10:44:07 -0400

    feat: :sparkles: Update player usernames in the tumbling player on join to account for username changes

--- src/main/kotlin/xyz/devcmb/tumblers/Constants.kt
+++ src/main/kotlin/xyz/devcmb/tumblers/Constants.kt
@@ -2,6 +2,6 @@ package xyz.devcmb.tumblers
 
 object Constants {
     val IS_DEVELOPMENT = true
-    const val VERSION: String = "09f"
+    const val VERSION: String = "617"
     const val BRANCH: String = "game/flood_escape"
 }
