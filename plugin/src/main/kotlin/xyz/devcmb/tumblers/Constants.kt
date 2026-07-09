51a8d15d9e49c7c5d0387e2d96efbf1cece16db9
29cmb <devcmb@outlook.com>
2026-07-09 15:05:38 -0400

    docs: :memo: Add more documentation to the confi.yml file

--- plugin/src/main/kotlin/xyz/devcmb/tumblers/Constants.kt
+++ plugin/src/main/kotlin/xyz/devcmb/tumblers/Constants.kt
@@ -2,6 +2,6 @@ package xyz.devcmb.tumblers
 
 object Constants {
     val IS_DEVELOPMENT = true
-    const val VERSION: String = "6eb"
+    const val VERSION: String = "6ed"
     const val BRANCH: String = "master"
 }
