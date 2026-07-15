15dd1200dfad6b3abfa0dcc55558460535ecc198
29cmb <devcmb@outlook.com>
2026-07-15 13:34:55 -0400

    feat: :sparkles: Retexture glass panes and add a basically empty map for tower ascent

--- plugin/src/main/kotlin/xyz/devcmb/tumblers/controllers/games/tower_ascent/TowerAscentScoreboard.kt
+++ plugin/src/main/kotlin/xyz/devcmb/tumblers/controllers/games/tower_ascent/TowerAscentScoreboard.kt
@@ -2,9 +2,14 @@ package xyz.devcmb.tumblers.controllers.games.tower_ascent
 
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
+import org.bukkit.entity.Player
+import xyz.devcmb.tumblers.engine.GameData
 import xyz.devcmb.tumblers.ui.scoreboard.HandledScoreboard
 
-class TowerAscentScoreboard : HandledScoreboard.GameScoreboard(TowerAscentData, NamedTextColor.LIGHT_PURPLE) {
+class TowerAscentScoreboard(
+    val player: Player,
+    gameData: GameData
+) : HandledScoreboard.GameScoreboard(gameData, NamedTextColor.LIGHT_PURPLE) {
     override val id: String = "towerAscentScoreboard"
     override fun getLines(): ArrayList<Component> {
         return arrayListOf()
