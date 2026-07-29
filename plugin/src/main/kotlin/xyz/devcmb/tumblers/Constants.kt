f1225e13818bbd0118ac1114858ec03c150f37f5
29cmb <devcmb@outlook.com>
2026-07-29 17:56:13 -0400

    fix: :bug: Turn back on the small enclosed room

--- plugin/src/main/kotlin/xyz/devcmb/tumblers/Constants.kt
+++ plugin/src/main/kotlin/xyz/devcmb/tumblers/Constants.kt
@@ -2,6 +2,6 @@ package xyz.devcmb.tumblers
 
 object Constants {
     val IS_DEVELOPMENT = true
-    const val VERSION: String = "834"
+    const val VERSION: String = "835"
     const val BRANCH: String = "master"
 }
